35f12845898c3b8bf6f024f95cbd749d8c3c6007
Peter Georgas <peter_georgas@yahoo.com>
2022-10-30 00:23:55 -0400

    Added firebase calendar storage

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import Settings from "./components/Settings/Settings";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
-	return <div>
-		<Routes>
-			<Route path = "/login" element = {<Login/>}/> 	
-			<Route path = "/settings" element = {<Settings/>}/> 	
-
-			</Routes> 
-	</div>;
+	return (
+		<div>
+			<Routes>
+				<Route path="/login" element={<Login />} />
+				<Route path="/settings" element={<Settings />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
