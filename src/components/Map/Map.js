f79d3bf7c3b7d3afea59af676f3935a14c9c13cf
SilveredBird <81721279+SilveredBird@users.noreply.github.com>
2022-12-06 00:50:34 -0500

    Styling changes
    Styling changes for apps

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,35 +1,69 @@
 import {
     Box,
 } from '@chakra-ui/react';
+import React, { useEffect, useState, useRef } from "react";
+import { auth } from "../../firebase/firebase.js";
+import { useAuthState } from "react-firebase-hooks/auth";
+import { GoogleAuthProvider } from "firebase/auth";
+import {getSettingsByUserId} from "../../firebase/firebase";
 
 function Map() {
 
+
+    const [user] = useAuthState(auth);
+    const [startLocation, setStartLocation] = useState();
+    const [endLocation, setEndLocation] = useState();
+    
+
+    useEffect(() => {
+        if (user) {
+            getSettingsByUserId(user.uid).then((settings) => {
+                if (settings.startLocation != undefined) {
+                    setStartLocation(settings.startLocation);
+                    console.log(startLocation);
+                }
+                if (settings.endLocation != undefined) {
+                    setEndLocation(settings.endLocation);
+                    console.log(endLocation);
+                }
+            });
+        }
+    }, [startLocation]);
+    const starturl = `https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=1500&type=restaurant&keyword=cruise&key=AIzaSyBzkJwRb9PUctHdPWU66dVgX6lpY288Fts `;
+
+     
+    useEffect(() => {
+            fetch(starturl, {
+                headers: {
+                    "access-control-allow-credentials": "true",
+                    "Access-Control-Allow-Origin": "true"
+                }})
+                .then((resp) => resp.json())
+                .then((json) => {
+                    console.log(json);
+                    setAllInfo(json.data);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+    }, []);
+    
+     
     const originid = "ChIJScxbM7-WMIgRZS4whoS0Zzw";
     const destid = "ChIJn6btdMp4PIgRdbWVrQ87x_k";
 
     const srcstring = `https://www.google.com/maps/embed/v1/directions?origin=place_id:${originid}&destination=place_id:${destid}&key=AIzaSyBzkJwRb9PUctHdPWU66dVgX6lpY288Fts `;
 
     return (
-        <div bg="gray" h="100vh">
             <Box
                 as="iframe"
-                title="Work Route"
-                border="2px"
-                borderColor="gray.300"
-                borderRadius="md"
                 bg="black"
                 color="white"
-                w="480px"
-                h="480px"
-                padding="0"
-                style={{ border: 0 }}
-                loading="lazy"
-                allowFullScreen=""
+                h='100%'
+                w='100%'
                 src={srcstring}
-
             >
             </Box>
-        </div>
     );
 }
 
